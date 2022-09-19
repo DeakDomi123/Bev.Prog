b85a07d27b29163f93e37a126e4dcef348822eaa
Dominik Deák <deakdomi1024@gmail.com>
2022-09-19 20:36:52 +0200

    Signed-off-by: Dominik Deák <deakdomi1024@gmail.com>

--- Python/Hazi1.py
+++ Python/Hazi1.py
@@ -1,17 +1,22 @@
-print("Adjon meg egy mondatot:")
-bekertmondat = input()
-szotar = {
+def szotarfeltoltes(bekertmondat):
+    for x in bekertmondat:
+        if (szotar.__contains__(x)):
+            szotar[x] += 1
+        else:
+            szotar[x] = 1
 
-}
+def bekeres():
+    print("Adjon meg egy mondatot:")
+    bekertmondat = input()
+    return bekertmondat
 
-for x in bekertmondat:
-    if (szotar.__contains__(x)):
-        szotar[x] += 1
-    else:
-        szotar[x] = 1
+if __name__ == "__main__":
+    bekertmondat = bekeres()
+    szotar = {
 
-print("Betűk gyakorisága: ",szotar)
-print("Fordítva: ",bekertmondat[::-1])
-
-lista = bekertmondat.split(' ')
-print("Listába rendezve szavanként: ",lista)
+    }
+    szotarfeltoltes(bekertmondat)
+    print("Betűk gyakorisága: ",szotar)
+    print("Fordítva: ",bekertmondat[::-1])
+    lista = bekertmondat.split(' ')
+    print("Listába rendezve szavanként: ",lista)
--- Python/Hazi2.py
+++ Python/Hazi2.py
@@ -1,10 +1,17 @@
-print("Adjon meg egy számot és egy mértékegységet (cm/inch):")
-szam = input()
-mertekegyseg = input()
+def bekeres():
+    print("Adjon meg egy számot és egy mértékegységet (cm/inch):")
+    szam = int(input())
+    mertekegyseg = input()
+    return szam,mertekegyseg
 
-if (mertekegyseg != "inch" and mertekegyseg != "cm"):
-    print("Not correct unit!")
-elif (mertekegyseg == "cm"):
-    print(round((float(szam)*0.393700787),2)," inches")
-elif (mertekegyseg == "inch"):
-    print(round((float(szam)*2.54),2)," cm")
+def kiszamolo(szam, mertekegyseg):
+    if (mertekegyseg == "cm"):
+        print(round((szam*0.393700787),2)," inches")
+    elif (mertekegyseg == "inch"):
+        print(round((szam*2.54),2)," cm")
+    elif (mertekegyseg != "inch" and mertekegyseg != "cm"):
+        print("Not correct unit!")
+
+if __name__ == "__main__":
+    szam,mertekegyseg = bekeres()
+    kiszamolo(szam,mertekegyseg)
